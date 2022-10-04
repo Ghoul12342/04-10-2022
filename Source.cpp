3ca96ade10fcfd5591f2e372b0628aec96e4696d
Ghoul12342 <114170053+Ghoul12342@users.noreply.github.com>
2022-10-04 18:42:22 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,159 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "russian");
+	//int month;
+	//cin >> month;
+	//switch (month) {
+	//case 1:
+	//case 2:
+	//case 12:cout << "«има" << endl; break;
+	//case 3:
+	//case 4:
+	//case 5:cout << "весна" << endl; break;
+	//case 6:
+	//case 7:
+	//case 8:cout << "лето" << endl; break;
+	//case 9:
+	//case 10:
+	//case 11:cout << "осень" << endl; break;
+	//}
+	//int month;
+	//cin >> month;
+	//switch (month) {
+	//case 1:cout << "январь = 31" << endl; break;
+	//case 2:cout << "‘евраль = 28" << endl; break;
+	//case 12:cout << "ƒекабрь = 30" << endl; break;
+	//case 3:cout << "ћарт = 31" << endl; break;
+	//case 4:cout << "јпрель = 30" << endl; break;
+	//case 5:cout << "ћай = 31" << endl; break;
+	//case 6:cout << "»юнь = 30" << endl; break;
+	//case 7:cout << "»юль = 31" << endl; break;
+	//case 8:cout << "јвгуст = 31" << endl; break;
+	//case 9:cout << "—ент€брь = 30" << endl; break;
+	//case 10:cout << "ќкт€брь = 31" << endl; break;
+	//case 11:cout << "Ќо€бь = 31" << endl; break;
+	//}
+
+
+	//Case5.јрифметические действи€ над числами пронумерованы следующим
+	//образом : 1 Ч сложение, 2 Ч вычитание, 3 Ч умножение, 4 Ч деление.ƒан
+	//номер действи€ N(целое число в диапазоне 1Ц4) и вещественные числа A
+	//и B(¬ не равно 0).¬ыполнить над числами указанное действие и вывести
+	//результат
+		/*double A, B;
+		cin >> A >> B;
+		int Ad;
+		cin >> Ad;
+
+		switch (Ad) {
+		case 1: cout << A + B << endl; break;
+		case 2: cout << A - B << endl; break;
+		case 3: cout << A * B << endl; break;
+		case 4: cout << A / B << endl; break;
+		default: cout << "ошибка" << endl;*/
+
+
+		// 1
+			/*int a;
+			cin >> a;
+			if (a == 10)
+				cout << "Verno" << endl;
+			else
+				cout << "neverno" << endl;*/
+				// 1
+				//int a;
+				//cin >> a;
+				//if (a == 0)
+				//	cout << "Verno" << endl;
+				//else
+				//	cout << "NeVerno" << endl;
+
+				//2
+				/*int a;
+				cin >> a;
+				if (a > 0)
+					cout << "Verno" << endl;
+				else
+					cout << "NeVerno" << endl;*/
+
+					//3
+					/*int a;
+					cin >> a;
+					if (a < 0)
+						cout << "Verno" << endl;
+					else
+						cout << "NeVerno" << endl;*/
+
+						//4
+						/*int a;
+						cin >> a;
+						if (a >= 0)
+							cout << "Verno" << endl;
+						else
+							cout << "NeVerno" << endl;*/
+
+							//5
+							/*int a;
+							cin >> a;
+							if (a <= 0)
+								cout << "Verno" << endl;
+							else
+								cout << "NeVerno" << endl;*/
+
+								// 6
+								/*int a;
+								cin >> a;
+								if (a != 0)
+									cout << "Verno" << endl;
+								else
+									cout << "NeVerno" << endl;*/
+
+	//9
+/*	bool test = true;
+	if (test == true) {
+		cout << "verno" << endl;
+	}
+	if (test != true) {
+		cout << "Neverno" << endl;
+	*/
+
+	//11
+	//int a;
+	//cin >> a;
+	//if (a > 0 && a < 5)
+	//	cout << "Verno" << endl;
+	//else
+	//	cout << "Neverno" << endl;
+
+	//12
+	/*double a;
+	cin >> a;
+	if (a == 0 || a == 2) {
+		a = a + 7;
+		cout << a << endl;
+	}
+	else
+		a = a / 10; {
+		cout << a << endl; }*/
+
+//13
+//int a, b;
+//	cin >> a >> b;
+//if (a <= 1 && b >= 3)
+//	cout << a + b << endl;
+//else
+//	cout << a - b << endl;
+//	
+//13
+int a, b;
+	cin >> a >> b;
+	if ((a > 2 && a < 11) || (b >= 6 && a < 14))
+		cout << "Verno " << endl;
+	else
+	cout << "Neverno" << endl;
+}
+	
+
+
